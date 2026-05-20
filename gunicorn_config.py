360c02a8894eb2a1a797e23aeaa5a463d0f659ac
Slopa <slopa@live.com>
2026-05-21 01:24:51 +0300

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -9,7 +9,7 @@ bind = f"0.0.0.0:{os.environ.get('PORT', '8000')}"
 backlog = 2048
 
 # Worker processes
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = 1
 worker_class = 'sync'
 worker_connections = 1000
 timeout = 120
